78d968809955e5f02d53fe3bdd761c41835403b7
msamiluludag <muhammed649@gmail.com>
2021-06-20 02:14:41 +0300

    added custom properties to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
   purge: ["index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          orange: "hsl(31, 77%, 52%)",
+          cyan: "hsl(184, 100%, 22%)",
+          darkCyan: "hsl(179, 100%, 13%)",
+          neutralGray: "hsl(0, 0%, 95%)",
+        },
+      },
+    },
+    fontFamily: {
+      lexendDeca: ["Lexend Deca", "sans-serif"],
+      bigShouldersDisplay: ["Big Shoulders Display", "sans-serif"],
+    },
+    fontSize: {
+      15: ["15px", "1.4rem"],
+    },
   },
   variants: {
     extend: {},
