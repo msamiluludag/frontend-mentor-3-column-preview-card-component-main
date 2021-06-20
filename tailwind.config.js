9615b6a96275573eba841cb6577202a8b25d363c
msamiluludag <muhammed649@gmail.com>
2021-06-20 03:47:18 +0300

    added color, fontSize and maxWidth properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,32 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          orange: "hsl(31, 77%, 52%)",
-          cyan: "hsl(184, 100%, 22%)",
-          darkCyan: "hsl(179, 100%, 13%)",
+          orange: {
+            dark: "#BE6E18",
+            light: "hsl(31, 77%, 52%)",
+          },
+          cyan: {
+            dark: "#00393D",
+            light: "hsl(184, 100%, 22%)",
+          },
+
+          darkCyan: {
+            dark: "#000F0F",
+            light: "hsl(179, 100%, 13%)",
+          },
           neutralGray: "hsl(0, 0%, 95%)",
         },
+      },
+      fontSize: {
+        15: ["15px", "1.4rem"],
+      },
+      maxWidth: {
+        card: "920px",
       },
     },
     fontFamily: {
       lexendDeca: ["Lexend Deca", "sans-serif"],
       bigShouldersDisplay: ["Big Shoulders Display", "sans-serif"],
-    },
-    fontSize: {
-      15: ["15px", "1.4rem"],
     },
   },
   variants: {
